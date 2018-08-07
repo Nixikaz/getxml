91c61832a89ed68196d4639bde616017c403bcda
Nixikaz <39746716+Nixikaz@users.noreply.github.com>
2018-08-06 18:15:57 -0700

    Add files via upload
    trying to get rework to work

--- getxml.js
+++ getxml.js
@@ -1,5 +1,4 @@
-﻿window.onload = function() { 
-var xhttp = false;
+﻿var xhttp = false;
 const shiftArray = ["1st", "2nd"];
 const stageArray = ["HV-FRF2", "HV4-ASSY-B", "HV5-BBOND1",
                     "HV5-ASSY-F", "HV-FSBOND1", "HV6QX-INI",
@@ -16,7 +15,7 @@ function xRequest(stage, url) {
         alert('Cannot create an XMLHTTP instance');
         return false;
     }
-    xhttp.onreadystatechange = function () {
+    xhttp.onload = function () {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             getXml(stage, this);
         }
@@ -24,23 +23,39 @@ function xRequest(stage, url) {
     xhttp.open('GET', url + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime(), true);
     xhttp.send(null);
 }
-xRequest(stageArray[2], 'EfficiencyDashboard.xml');
+var plan = 600;
+document.getElementById(stageArray[0] + "Plan").innerHTML = plan;
 function getXml(stage, xml) {
     var xmldoc = xml.responseXML;
     var partCount = 0;   
-    hourNode = xmldoc.getElementsByTagName("HOUR");    
-    titleNode = xmldoc.getElementsByTagName("TITLE");
     stageNode = xmldoc.getElementsByTagName("STAGE");
-    shiftNode = xmldoc.getElementsByTagName("SHIFT");
-    for (let i = 0; i < hourNode.length; i++) {
-        let hourNodeValue = hourNode[i].childNodes[0].nodeValue;
-        let titleNodeValue = titleNode[i].childNodes[0].nodeValue;
-        let stageNodeValue = stageNode[i].childNodes[0].nodeValue;
-        let shiftNodeValue = shiftNode[i].childNodes[0].nodeValue;
-    if (hourNodeValue == 3 && shiftNodeValue === "1st" && stageNodeValue === stage) { 
-            partCount++;
+    reworkNode = xmldoc.getElementsByTagName("REWORKCODES");
+    for (let i = 0; i < stageNode.length; i++) {
+        let stageNodeValue = stageNode[i].childNodes[0].textContent;
+        if (/*reworkNodeValue && */stageNodeValue === stage) { 
+            partCount++;            
         }
-    }      
-    document.getElementById("RFAActual").innerHTML = partCount;
+    }  
+    countReworkCodes(stage, reworkNode);
+    var effPercentage = partCount / plan * 100;
+    document.getElementById(stage + "Eff").innerHTML = effPercentage.toFixed(2) + "%";
+    document.getElementById(stage).innerHTML = partCount;
+    
 }
+function countReworkCodes(stage, nodeArg) {
+    let reworkCount = 0;
+    for (let i = 0; i < nodeArg.length; i++) {
+        let reworkNodeValue = reworkNode[i].childNodes[0].nodeValue;
+        if (reworkNodeValue) {
+            reworkCount++;
+            // while (stage) {
+            //     reworkCount--;
+            // }
+        }
+        
+        document.getElementById(stage + "Rejects").innerHTML = reworkCount;
+    }
 }
+
+
+xRequest(stageArray[0], 'EfficiencyDashboard.xml');
